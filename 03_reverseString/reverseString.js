3328f633ed44a14b27816eb1c81dbefbfd277c1e
Van Spina <89276523+palmtreeshinobi@users.noreply.github.com>
2023-12-11 13:42:05 -0500

    completed 2 and 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,21 @@
-const reverseString = function() {
+const reverseString = function(strIn) {
+    let strLength = strIn.length;
+    let strArr = [];
+    let outStr = '';
 
+    // Populate the array with characters from the string
+    for (let counter = 0; counter < strLength; counter++) {
+        strArr[counter] = strIn.charAt(counter);
+    }
+
+    // Build the reversed string
+    for (let i = strLength - 1; i >= 0; i--) {
+        outStr += strArr[i];
+    }
+
+    return outStr;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
