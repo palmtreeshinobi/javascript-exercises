2e09734dc2aeb9ac8e6947ffcfd93b3f9c9be9c6
Van Spina <89276523+palmtreeshinobi@users.noreply.github.com>
2023-12-11 13:24:22 -0500

    completed exercise 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let n = 0;
+    let result = '';
+    if (num < 0){
+        return "ERROR";
+    }
+    while (n < num) {
+        result += string;
+        n++;
+    }
+    return result;
 };
 
 // Do not edit below this line
